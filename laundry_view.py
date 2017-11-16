d0556ee53fb13dd4ff60077a38c634d2b4705f95
krpuncy <=>
2017-11-16 14:51:08 -0700

    finished collect_room and testable with __main__

--- laundry_view.py
+++ laundry_view.py
@@ -2,38 +2,51 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 laundry_url = "http://m.laundryview.com"
 
+tags = ["est. time remaining n min","extended cycle running for n mins",\
+        "cycle ended n minutes ago","out of service","unknown",\
+        "cycle has ended - door still closed","available"]
+
+def heat_soup(url):
+    """ grabs the soup from the url """
+    campus_html = urlopen(url).read()
+    return BeautifulSoup(campus_html,"html.parser")
+
+def clean(string):
+    """ cleans and returns the string given """
+    return string.replace('\t','').replace('\n','').replace('  ','')
+
 def collect_campus():
-    """ returns a dictionary of campus locations """
+    """ returns a dictionary of campus locations {url:name}"""
     soup = BeautifulSoup(urlopen(laundry_url).read(),"html.parser")
     campus_locations = {}
+
     for link in soup.find_all('a'):
         if "lvs" in link['href']:
-            location_name = link.string.replace("  ", "").replace("\n","")
+            location_name = clean(link.string)
             location_link = link['href']
-            campus_locations[location_name] = location_link
+            campus_locations[location_link] = location_name
+
     return campus_locations
 
 def collect_buildings(campus_url):
-    """ returns a dictionary of buildings """
+    """ returns a dictionary of buildings {url:name}"""
     buildings = {}
-    campus_html = urlopen(laundry_url+"/"+campus_url).read()
-    campus_soup = BeautifulSoup(campus_html,"html.parser")
+    campus_soup = heat_soup(laundry_url+"/"+campus_url)
+
     for item in campus_soup.find_all('li'):
         a_tags = item.find_all('a')
         # remove the last two values [home,options]
         for a in a_tags:
             if "Home" in a.string or "Options" in a.string:
                 continue
-            buildings[a['href']] = a.string.replace("  ","")
+            buildings[a['href']] = clean(a.string)
+
     return buildings
 
-def clean(string):
-    return string.replace('\t','').replace('\n','').replace('  ','')
-
 def collect_room(room_url):
-    room = {}
-    room_html = urlopen("http://classic.laundryview.com/"+room_url).read()
-    room_soup = BeautifulSoup(room_html,"html.parser")
+    """ returns a list of stats for the room """
+    room  = {}
+    room_soup = heat_soup("http://classic.laundryview.com/"+room_url)
 
     stats = room_soup.find('div',{'class':'monitor-total'})
     dryer_available = stats.contents[2].split(" ")[1:4]
@@ -44,3 +57,29 @@ def collect_room(room_url):
 
     washers = room_soup.find('td',{'align':'right','class':'bgwhite'})
     washers = [clean(stat.string) for stat in washers.find_all('span',{'class':'stat'})]
+
+    room['dryer'] = [dryer_available, dryers]
+    room['washer'] = [washer_available,washers]
+
+    return room
+
+if __name__ == "__main__":
+    campus = collect_campus()
+    buildings = {}
+    room = {}
+
+    # print the all the buildings in the first campus
+    for url, name in campus.items():
+        print(name)
+        buildings = collect_buildings(url)
+        print(buildings,"\n")
+        break
+
+    #print the floors in the fist building in the list
+    for url, name in buildings.items():
+        print(name)
+        room = collect_room(url)
+        break
+
+    print(room['dryer'])
+    print(room['washer'])
